adc61efed4587ba1b4c4c7248b6b2881e5a80408
Shubham Nayak <shubhamnayak0904@gmail.com>
2022-10-20 09:08:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,96 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App()
+ {
+  return (
+    <div className="App">
+    
+    <table> 
+      <tr>
+      <th class="rotate">1/2022</th>
+      <th>2/2022</th>
+      <th>3/2022</th>
+      <th>4/2022</th>
+      <th>5/2022</th>
+      <th>6/2022</th>
+      <th>7/2022</th>
+      <th>8/2022</th>
+      <th>9/2022</th>
+      <th>10/2022</th>
+      <th>11/2022</th>
+      <th>12/2022</th>
+      <th>13/2022</th>
+      <th>14/2022</th>
+      <th>15/2022</th>
+      <th>16/2022</th>
+      <th>17/2022</th>
+      <th>18/2022</th>
+      <th>19/2022</th>
+      <th>20/2022</th>
+      <th>21/2022</th>
+      <th>22/2022</th>
+      <th>23/2022</th>
+      <th>24/2022</th>
+      <th>25/2022</th>
+      <th>26/2022</th>
+      <th>27/2022</th>
+      <th>28/2022</th>
+      <th>29/2022</th>
+      <th>30/2022</th>
+      </tr>
+
+      <tr>
+        <th>Januray</th>
+        <th> </th>
+      </tr>
+
+      <tr>
+        <th>Feburary</th>
+        <th> </th>
+      </tr>
+
+      <tr>  
+        <th>March</th>
+      </tr>
+       <tr>
+        <th>April</th>
+       </tr>
+
+       <tr> 
+        <th>May</th>
+       </tr>
+
+       <tr> 
+        <th>June</th>
+       </tr>
+
+       <tr> 
+        <th>July</th>
+       </tr> 
+
+       <tr>
+        <th>August</th>
+       </tr>
+
+       <tr> 
+        <th>September</th>
+       </tr>
+       <tr>
+        <th>October</th>
+       </tr>
+
+       <tr> 
+        <th>November</th>
+       </tr>
+
+       <tr> 
+        <th>December</th>
+      </tr>
+    </table>
+       
+    </div>
+  );
+}
+
+export default App;
